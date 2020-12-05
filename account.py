46e72bc2633c19b6af8b84c8531e61396232089f
git <git@email.com>
2020-12-05 12:40:00 +0530

    3

--- account.py
+++ account.py
@@ -1,10 +1,1 @@
-  
-# Function to demonstrate printing pattern triangle 
-        for j in range(0, i+1): 
-          
-            # printing stars 
-            print("* ", end="") 
-      
-        # ending line after each row 
-        print("\r") 
-
+print('Hello, world!')
